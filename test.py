f0bf08229c61533c8dc1d74991fcd1a1f2286723
AjwadA <56668785+AjwadA@users.noreply.github.com>
2021-12-06 12:09:19 +0000

    added tello example from dr's

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import numpy as np
 
 print("hello gang");
-
+# aq rasa for package, dia just main import2 jer. tadi aq main copy code numpy dia run
+# perggh amazing, nice ah x leceh mcm pycharm haha. tp mungkin limited kot apa yg boleh import? oo yg version control ni ea, niize Selamat bersantap. # hahah. tu al x tahu lagi limit dia ape. pastu nk push/commit senang kot
+# haah. well aq nk gi makan dulu. have fun
